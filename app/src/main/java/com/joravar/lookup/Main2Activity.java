157c1e9c0fd142f05f48a7b58034ef7b9e109b6e
ravi joshi <ravi joshi>
2017-03-01 22:31:09 +0530

    trans

--- app/src/main/java/com/joravar/lookup/Main2Activity.java
+++ app/src/main/java/com/joravar/lookup/Main2Activity.java
@@ -1,13 +1,92 @@
 package com.joravar.lookup;
 
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.os.Environment;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
-public class Main2Activity extends AppCompatActivity {
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import static android.R.attr.bitmap;
+
+public class Main2Activity extends Activity {
+     Bitmap bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        Toast.makeText(Main2Activity.this,"Phone hang",Toast.LENGTH_SHORT).show();
+
     }
+
+    @Override
+    protected void onStart() {
+
+        View v =getWindow().getDecorView().getRootView();
+        v.setDrawingCacheEnabled(true);
+
+// this is the important code :)
+// Without it the view will have a dimension of 0,0 and the bitmap will be null
+        v.measure(View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED),
+                View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
+        v.layout(0, 0, v.getMeasuredWidth(), v.getMeasuredHeight());
+
+        v.buildDrawingCache(true);
+        bitmap = Bitmap.createBitmap(v.getDrawingCache());
+        v.setDrawingCacheEnabled(false); // clear drawing ca
+        Log.e("Bitmap",bitmap+"");
+
+       TextRecognizer textRecognizer = new TextRecognizer.Builder(getApplicationContext()).build();
+        if(!textRecognizer.isOperational())
+            Log.e("error","dependencies not available");
+        else {
+            Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+            Log.e("frame set ",5+"");
+            SparseArray<TextBlock> items = textRecognizer.detect(frame);
+            StringBuilder stringBuilder = new StringBuilder();
+            Log.e("Values",items.size()+"");
+            for (int i = 0; i < items.size(); i++) {
+                TextBlock item = items.valueAt(i);
+                stringBuilder.append(item.getValue());
+                stringBuilder.append("\n");
+            }
+            Log.e("frame set ",8+"");
+            String newText = stringBuilder.toString().toLowerCase().trim();
+            Log.e("text== ",newText);
+        }
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+
 }
--- app/src/main/java/com/joravar/lookup/Myservice.java
+++ app/src/main/java/com/joravar/lookup/Myservice.java
@@ -2,12 +2,16 @@ package com.joravar.lookup;
 
 import android.app.Service;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Paint;
 import android.graphics.PixelFormat;
 import android.graphics.Point;
+import android.graphics.Rect;
 import android.os.Build;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
+import android.text.TextPaint;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.MotionEvent;
@@ -26,6 +30,10 @@ public class Myservice extends Service {
     private WindowManager windowManager;
     private ImageButton chatHead;
     WindowManager.LayoutParams params;
+    private Paint mRectPaint;
+    private boolean mDrawRect = false;
+    private TextPaint mTextPaint = null;
+    int check = 1;
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
@@ -42,7 +50,6 @@ public class Myservice extends Service {
     public int onStartCommand(Intent intent, int flags, int startId) {
         Toast.makeText(this,"Service is started",Toast.LENGTH_SHORT).show();
         windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
-
         chatHead = new ImageButton(this);
         chatHead.setImageResource(R.drawable.pt2);
         chatHead.setClickable(true);
@@ -68,24 +75,37 @@ public class Myservice extends Service {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
               Log.e("touching","");
-                switch (event.getAction()) {
-                    case MotionEvent.ACTION_DOWN:
-                        initialX = params.x;
-                        initialY = params.y;
-                        initialTouchX = event.getRawX();
-                        initialTouchY = event.getRawY();
-                        return true;
-                    case MotionEvent.ACTION_UP:
-                        return true;
-                    case MotionEvent.ACTION_MOVE:
-                        params.x = initialX
-                                + (int) (event.getRawX() - initialTouchX);
-                        params.y = initialY
-                                + (int) (event.getRawY() - initialTouchY);
-                        windowManager.updateViewLayout(chatHead, params);
-                        return true;
-                }
-                return false;
+
+                    switch (event.getAction()) {
+                        case MotionEvent.ACTION_DOWN:
+                            initialX = params.x;
+                            initialY = params.y;
+                            initialTouchX = event.getRawX();
+                            initialTouchY = event.getRawY();
+                            Intent in = new Intent();
+                            in.setClass(Myservice.this,Main2Activity.class);
+                            in.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                            startActivity(in);
+                            return true;
+                        case MotionEvent.ACTION_MOVE:
+                            params.x = initialX
+                                    + (int) (event.getRawX() - initialTouchX);
+                            params.y = initialY
+                                    + (int) (event.getRawY() - initialTouchY);
+                            windowManager.updateViewLayout(chatHead, params);
+                            return true;
+                        case MotionEvent.ACTION_UP:
+                            check=0;
+
+                            Toast.makeText(Myservice.this, " Please select the text ", Toast.LENGTH_SHORT).show();
+                            return true;
+
+                    }
+
+
+
+                    return false;
+
             }
         });
         windowManager.addView(chatHead, params);
@@ -100,4 +120,6 @@ public class Myservice extends Service {
             windowManager.removeView(chatHead);
 
     }
+
+
 }
